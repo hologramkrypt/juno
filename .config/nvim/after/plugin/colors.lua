0a13883e2302b9e98124b35f58ae0c6a9c7b8bcc
SHADE <iamtheravenhq@gmail.com>
2025-10-18 20:05:26 -0700

    Add files via upload

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,13 @@
+function ColorMyPencils(color)
+	color = ""
+    vim.api.nvim_set_option('background', 'dark')
+    vim.cmd.colorscheme('github_dark_tritanopia')
+	
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+
+end
+
+ColorMyPencils()
+
+
